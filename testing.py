73c5236df793c60620c4e4b40773463b60a2b1ad
Sri-code23 <sridharansri2312@gmail.com>
2025-03-23 15:08:42 +0530

    uploaded the files

--- testing.py
+++ testing.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 model = YOLO("best.pt")  # Ensure "best.pt" is in the project folder
 
 # Perform fall detection on an image
-results = model.predict("sample.jpg", save=True)
+results = model.predict("sample2.jpg", save=True)
 
 # Output the results
 print("✅ Detection complete! Check the 'runs/detect/predict/' folder.")
